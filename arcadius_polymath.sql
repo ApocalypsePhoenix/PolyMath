910c606eaa9b986d6271cabbd6fa3f3f616e9fc7
ApocalypsePhoenix <69609445+ApocalypsePhoenix@users.noreply.github.com>
2026-04-06 22:25:45 +0800

    Update arcadius_polymath.sql

--- arcadius_polymath.sql
+++ arcadius_polymath.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 05, 2026 at 03:29 PM
+-- Generation Time: Apr 06, 2026 at 07:43 PM
 -- Server version: 11.4.8-MariaDB-log
 -- PHP Version: 8.4.19
 
@@ -99,7 +99,17 @@ INSERT INTO `questions` (`question_id`, `level_id`, `quiz_id`, `question_text`, 
 (7, 2, 1, 'Test Q7', '1234', '64543', '3524', '342', 'B', 'xfgfbd', 3, NULL, NULL),
 (8, 2, 1, 'Test Q8', '46', '3324', '453', '656', 'C', 'jhgfred', 3, NULL, NULL),
 (9, 2, 1, 'Test Q9', '6454', '321', '234', '54353', 'B', 'nhbgfd', 3, NULL, NULL),
-(11, 2, 1, 'Test Q10', '3423', '21', '553543', '3123', 'B', 'dhgfgsdg', 3, NULL, NULL);
+(11, 2, 1, 'Test Q10', '3423', '21', '553543', '3123', 'B', 'dhgfgsdg', 3, NULL, NULL),
+(12, 2, 2, 'Q1', '1', '2', '3', '4', 'B', '1', 3, NULL, NULL),
+(13, 2, 2, 'Q2', '1', '2', '3', '4', 'C', '2', 3, NULL, NULL),
+(14, 2, 2, 'Q3', '1', '2', '3', '4', 'A', '3', 3, NULL, NULL),
+(15, 2, 2, 'Q4', '1', '2', '3', '4', 'B', '4', 3, NULL, NULL),
+(16, 2, 2, 'Q5', '1', '2', '3', '4', 'C', '5', 3, NULL, NULL),
+(17, 2, 2, 'Q6', '1', '2', '3', '4', 'D', '6', 3, NULL, NULL),
+(18, 2, 2, 'Q7', '1', '2', '3', '4', 'B', '7', 3, NULL, NULL),
+(19, 2, 2, 'Q8', '1', '2', '3', '4', 'A', '8', 3, NULL, NULL),
+(20, 2, 2, 'Q9', '1', '2', '3', '4', 'B', '9', 3, NULL, NULL),
+(21, 2, 2, 'Q10', '1', '2', '3', '4', 'A', '10', 3, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -122,7 +132,8 @@ CREATE TABLE `quizzes` (
 --
 
 INSERT INTO `quizzes` (`quiz_id`, `quiz_name`, `level_id`, `is_timed`, `time_limit`, `created_at`, `is_published`) VALUES
-(1, 'Test 1', 2, 0, 300, '2026-04-04 17:12:25', 1);
+(1, 'Quiz 1', 1, 0, 300, '2026-04-04 17:12:25', 1),
+(2, 'Quiz 2', 2, 1, 3600, '2026-04-05 12:57:46', 1);
 
 -- --------------------------------------------------------
 
@@ -138,6 +149,7 @@ CREATE TABLE `quiz_attempts` (
   `score` int(11) NOT NULL,
   `total_questions` int(11) NOT NULL,
   `time_taken_seconds` int(11) DEFAULT NULL,
+  `answers_json` longtext DEFAULT NULL,
   `completed_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
@@ -145,9 +157,8 @@ CREATE TABLE `quiz_attempts` (
 -- Dumping data for table `quiz_attempts`
 --
 
-INSERT INTO `quiz_attempts` (`attempt_id`, `user_id`, `level_id`, `quiz_id`, `score`, `total_questions`, `time_taken_seconds`, `completed_at`) VALUES
-(5, 3, 2, 1, 4, 10, 16, '2026-04-04 20:10:55'),
-(6, 3, 2, 1, 4, 10, 16, '2026-04-04 20:20:33');
+INSERT INTO `quiz_attempts` (`attempt_id`, `user_id`, `level_id`, `quiz_id`, `score`, `total_questions`, `time_taken_seconds`, `answers_json`, `completed_at`) VALUES
+(10, 4, 1, 1, 3, 10, 13, '[{\"qid\":\"7\",\"correct\":true,\"chosen\":\"B\"},{\"qid\":\"1\",\"correct\":false,\"chosen\":\"C\"},{\"qid\":\"6\",\"correct\":false,\"chosen\":\"A\"},{\"qid\":\"8\",\"correct\":false,\"chosen\":\"B\"},{\"qid\":\"5\",\"correct\":true,\"chosen\":\"D\"},{\"qid\":\"11\",\"correct\":false,\"chosen\":\"D\"},{\"qid\":\"4\",\"correct\":false,\"chosen\":\"B\"},{\"qid\":\"9\",\"correct\":false,\"chosen\":\"C\"},{\"qid\":\"3\",\"correct\":false,\"chosen\":\"A\"},{\"qid\":\"2\",\"correct\":true,\"chosen\":\"B\"}]', '2026-04-05 11:21:47');
 
 -- --------------------------------------------------------
 
@@ -185,32 +196,6 @@ CREATE TABLE `user_answers` (
   `is_correct` tinyint(1) NOT NULL,
   `chosen_option` char(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
---
--- Dumping data for table `user_answers`
---
-
-INSERT INTO `user_answers` (`answer_id`, `attempt_id`, `question_id`, `is_correct`, `chosen_option`) VALUES
-(31, 5, 3, 0, 'A'),
-(32, 5, 5, 0, 'B'),
-(33, 5, 4, 1, 'C'),
-(34, 5, 7, 0, 'D'),
-(35, 5, 9, 1, 'B'),
-(36, 5, 2, 1, 'B'),
-(37, 5, 1, 0, 'B'),
-(38, 5, 11, 0, 'A'),
-(39, 5, 8, 0, 'D'),
-(40, 5, 6, 1, 'C'),
-(41, 6, 7, 0, 'A'),
-(42, 6, 11, 1, 'B'),
-(43, 6, 8, 1, 'C'),
-(44, 6, 3, 0, 'D'),
-(45, 6, 1, 0, 'B'),
-(46, 6, 5, 0, 'B'),
-(47, 6, 6, 0, 'A'),
-(48, 6, 4, 1, 'C'),
-(49, 6, 9, 0, 'D'),
-(50, 6, 2, 1, 'B');
 
 --
 -- Indexes for dumped tables
@@ -264,9 +249,7 @@ ALTER TABLE `users`
 -- Indexes for table `user_answers`
 --
 ALTER TABLE `user_answers`
-  ADD PRIMARY KEY (`answer_id`),
-  ADD KEY `attempt_id` (`attempt_id`),
-  ADD KEY `question_id` (`question_id`);
+  ADD PRIMARY KEY (`answer_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -288,19 +271,19 @@ ALTER TABLE `levels`
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `quizzes`
 --
 ALTER TABLE `quizzes`
-  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `quiz_attempts`
 --
 ALTER TABLE `quiz_attempts`
-  MODIFY `attempt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `attempt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -312,7 +295,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user_answers`
 --
 ALTER TABLE `user_answers`
-  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -342,13 +325,6 @@ ALTER TABLE `quiz_attempts`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE SET NULL;
-
---
--- Constraints for table `user_answers`
---
-ALTER TABLE `user_answers`
-  ADD CONSTRAINT `user_answers_ibfk_1` FOREIGN KEY (`attempt_id`) REFERENCES `quiz_attempts` (`attempt_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
